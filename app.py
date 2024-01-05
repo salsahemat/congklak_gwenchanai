38e3e1077c1bc55d93b353783befcff3d23f6dca
Nuzulul-Salsabila <nsalsabila02@student.ciputra.ac.id>
2024-01-06 04:42:27 +0700

    return the app.py to tkinter not pygame anymore

--- app.py
+++ app.py
@@ -1,35 +1,16 @@
 import tkinter as tk
-
-import pygame
-from pygame.locals import *
-
 from congklakGame import CongklakGUI
 
 
 def main():
-  # # Membuat instance Tkinter untuk aplikasi GUI
-  #   root = tk.Tk()
+  # Membuat instance Tkinter untuk aplikasi GUI
+    root = tk.Tk()
 
-  #   # Membuat objek CongklakGUI yang akan menangani antarmuka pengguna
-  #   app = CongklakGUI(root)
+    # Membuat objek CongklakGUI yang akan menangani antarmuka pengguna
+    app = CongklakGUI(root)
 
-  #   # Memulai loop utama Tkinter untuk menampilkan GUI dan menunggu interaksi pengguna
-  #   root.mainloop()
-  pygame.init()
-  screen = pygame.display.set_mode((500, 500), HWSURFACE | DOUBLEBUF | RESIZABLE)
-  pic = pygame.image.load("asset/tempat2.png")
-  screen.blit(pygame.transform.scale(pic, (500, 500)), (0, 0))
-  pygame.display.flip()
-  while True:
-      pygame.event.pump()
-      event = pygame.event.wait()
-      if event.type == QUIT:
-          pygame.display.quit()
-      elif event.type == VIDEORESIZE:
-          screen = pygame.display.set_mode(
-              event.dict['size'], HWSURFACE | DOUBLEBUF | RESIZABLE)
-          screen.blit(pygame.transform.scale(pic, event.dict['size']), (0, 0))
-          pygame.display.flip()
+    # Memulai loop utama Tkinter untuk menampilkan GUI dan menunggu interaksi pengguna
+    root.mainloop()
     
-if __name__ == "__main__":
+if _name_ == "_main_":
     main()
